13c1ca1e98816695c5ecb366c684910f2646192f
Xavi <xavi@geekshubs.com>
2021-02-11 11:41:40 +0100

    Implemented interface

--- LUASOAP/src/com/lua/infrastructure/controller/LUAAPIImpl.java
+++ LUASOAP/src/com/lua/infrastructure/controller/LUAAPIImpl.java
@@ -1,5 +1,23 @@
 package com.lua.infrastructure.controller;
 
-public class LUAAPIImpl {
+import java.util.List;
+
+import javax.jws.WebService;
+
+import com.lua.domain.model.User;
+
+@WebService(endpointInterface = "com.lua.infrastructure.controller.LUAAPI")
+public class LUAAPIImpl implements LUAAPI {
+
+	@Override
+	public List<User> getUsers() {
+		// TODO Auto-generated method stub
+		return User.getUsers();
+	}
+
+	@Override
+	public void addUser(User user) {
+		 User.getUsers().add(user);
+	}
 
 }
