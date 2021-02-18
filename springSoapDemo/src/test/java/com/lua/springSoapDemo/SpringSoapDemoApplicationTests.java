2a3c7ad3fbffc66ceba449fdf7985ff650c7725f
Xavi <xavi@geekshubs.com>
2021-02-18 11:33:16 +0100

    create new project and andd dependencies

---
+++ springSoapDemo/src/test/java/com/lua/springSoapDemo/SpringSoapDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lua.springSoapDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoapDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
